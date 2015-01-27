d4cb4443abef11c9f28840d7df42d9cd4d4e92bd
Nick P <nb.piscitello@gmail.com>
2015-01-27 16:11:38 -0500

    Finalizing Sensor class - getTemp and waitUntilInTea are complete and ready to be tuned

--- src/auTEAmatic/Sensor.java
+++ src/auTEAmatic/Sensor.java
@@ -1,30 +1,38 @@
 package auTEAmatic;
 
 import lejos.nxt.SensorPort;
-import lejos.nxt.TouchSensor;
 
 public class Sensor {
 // This class handles all sensors used by the robot
 	
 	// fields - composed mostly of sensor values. Object creation is included here.
-	private int tempRaw;
-	private float tempAdjusted, tempMultiplier;
-	private boolean inTea;
-	TouchSensor conductivity = new TouchSensor(SensorPort.S1);
+	private static int tempRaw;
+	private static float tempAdjusted;
 	
 	// constructors - not needed, as objects of this class won't be necessary
 	
 	// methods - mostly returning sensor values and threshold decisions
-	public float getTemp()	{
-		// read raw value and adjust to Fahrenheit (or Celsius)
+	public static float getTemp()	{
+		// read raw value and adjust to Fahrenheit (or Celsius) - possibly add averaging if needed
 		tempRaw = SensorPort.S2.readRawValue();
 		// tempAdjusted = ? --> run the numbers <--
 		return tempAdjusted;
 	}
 	
-	public boolean isInTea()	{
-		// read conductivity sensor
-		inTea = conductivity.isPressed();
-		return inTea;
+	public void waitUntilInTea()	{
+		// read temperature probe spike - returns when the probe hits the water
+		while(true)	{
+			tempRaw = SensorPort.S2.readRawValue();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// Auto-generated catch block
+				e.printStackTrace();
+			}
+			// break loop if new sensor value is at least 25% higher than the old one
+			if(SensorPort.S2.readRawValue() >= tempRaw + (tempRaw * 0.25))
+			break;
+		}
+		return;
 	}
 }
